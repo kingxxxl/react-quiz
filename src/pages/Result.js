358fb464f22083b0917e2d45c6a5ef7a3b6a6c86
abdo <2030sav@gmail.com>
2022-06-26 21:26:52 +0300

    d

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 function Result({ score }) {
   return (
     <>
-      <Text alignSelf={"center"} fontSize={"1.5rem"} as="kbd">
+      <Text height={"100vh"} alignSelf={"center"} fontSize={"1.5rem"} as="kbd">
         {" "}
         The Result: {score} Points!
       </Text>
