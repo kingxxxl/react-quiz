d6bb10a124c3303b25b2011ee4efea35a0288d5e
abdo <2030sav@gmail.com>
2022-06-26 21:38:47 +0300

    d

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,12 +1,32 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Button, Link, Text, VStack } from "@chakra-ui/react";
 
 function Result({ score }) {
   return (
     <>
-      <Text height={"100vh"} alignSelf={"center"} fontSize={"1.5rem"} as="kbd">
-        {" "}
-        The Result: {score} Points!
-      </Text>
+      <VStack width={"100%"} height={"100vh"}>
+        <Box
+          w={"70%"}
+          my={209}
+          bg={"blue.200"}
+          border={"1px"}
+          borderRadius={10}
+          color={"BlackAlpha.700"}
+          textAlign={"center"}
+        >
+          <Text
+            my={9}
+            fontSize={"2rem"}
+            alignSelf={"center"}
+            fontSize={"1.5rem"}
+          >
+            The Result: {score} Points!
+          </Text>
+        </Box>
+
+        <Button w={"70%"} h={"5%"} colorScheme={"blackAlpha"}>
+          <Link href={"/"}>GO HOME!</Link>
+        </Button>
+      </VStack>
     </>
   );
 }
